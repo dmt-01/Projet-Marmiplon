889cfd4a5e11bda11432b8f2191e548e71c29127
indidi <indian.v01@gmail.com>
2025-07-31 16:58:13 +0200

    push inutile mais obligatoir

--- routes/index.ts
+++ routes/index.ts
@@ -1,9 +1,9 @@
-// import { Router } from "express";
+import { Router } from "express";
 
-// const router = Router();
+const router = Router();
 
-// router.get("/", (request, response) => {
-//   response.send("coucou")
-// });
+router.get("/", (request, response) => {
+  response.send("coucou")
+});
 
-// export default router;
+export default router;
