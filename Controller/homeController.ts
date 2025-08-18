a66551abc35fb679b6a222c4799de773a6b77492
Laurence-LaurencE <laurence20gros@gmail.com>
2025-08-18 16:55:20 +0200

    initialisation homeController.ts + modif nom Home.ejs

--- Controller/homeController.ts
+++ Controller/homeController.ts
@@ -1,17 +1,35 @@
 import { Controller } from "../libs/Controller";
 import { categories } from "../data";
+import { log } from "node:console";
 // def d'un tableau de données
 
 export class HomeController extends Controller {
   // va aller chercher les datas des 3 catégories ds data.ts
   public home() {
     const allCategories = categories;
+    // console.log(allCategories);
+    
 
-    this.response.render("pages/Acceuil", {
+    this.response.render("pages/Home", {
       categories: allCategories,
       title: "Marmiplon - Accueil",
     });
 
-    this.response.render("pages/Accueil");
-  }
+    this.response.sender("pages/Home");
+  
 }
+}
+
+/////////
+// public home() {
+//     const allCategories = categories;
+
+//     this.response.render("pages/Home", {
+//       categories: allCategories,
+//       title: "Marmiplon - Accueil",
+//     });
+
+//     this.response.render("pages/Home");
+//   }
+// }
+
--- routes/home.ts
+++ routes/home.ts
@@ -5,6 +5,7 @@ const home = Router();
 
 home.get("/", (request, response) => {
   const controller = new HomeController(request, response);
+  controller.home()
 });
 
 export default home;
