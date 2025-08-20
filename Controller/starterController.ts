8da89aa31b28fecbcfb46b75570891a56ae61c43
Tatiana77-git <tatiana.dor@free.fr>
2025-08-20 14:56:12 +0200

    starterController

--- Controller/starterController.ts
+++ Controller/starterController.ts
@@ -1,42 +1,57 @@
- import { Controller } from "../libs/Controller";
+  import { Controller } from "../libs/Controller";
 
- export class starterController extends Controller {
+const recettes = [
+  { id: 1, quantity: 1, unit: "tranche", ingredientId: 1, recipeId: 101 },
+  { id: 2, quantity: 2, unit: "unités", ingredientId: 2, recipeId: 101 },
+  { id: 3, quantity: 5, unit: "feuilles", ingredientId: 3, recipeId: 101 },
+  { id: 4, quantity: 2, unit: "cuillères à soupe", ingredientId: 4, recipeId: 101 },
+  { id: 5, quantity: 1, unit: "unité", ingredientId: 5, recipeId: 102 },
+  { id: 6, quantity: 2, unit: "unités", ingredientId: 2, recipeId: 102 },
+  { id: 7, quantity: 1, unit: "unité", ingredientId: 6, recipeId: 102 },
+  { id: 8, quantity: 200, unit: "grammes", ingredientId: 7, recipeId: 102 },
+  { id: 9, quantity: 10, unit: "unités", ingredientId: 8, recipeId: 102 }
+];
+  
+
+  export class starterController extends Controller {
 
 
-   public browseRecettes() {
-     this.response.render("pages/Recettes", {
-       message: "Voici la liste des recettes",
-      
-     });
-   }
+    public browseRecettes() {
+      this.response.render("pages/Recettes", {
+      data:recettes
+       });
+    }
 
-   public readRecette() {
-     const id = this.request.params.id;
-     console.log(id)
-     this.response.render("pages/Recettes", {
-      message: `Détails de la recette ${id}`,
-     });
-   }
+    public readRecette() {
+      const recetteId = this.request.params.id;
+      const selectedRecette = recettes.find (function (recette) {
+        return  recette.id === parseInt(recetteId)
+      })
+      this.response.render("pages/Recettes", {
+      data: selectedRecette
+      });
+    }
 
-   public editRecette() {
-     const id = this.request.params.id;
-   
-     this.response.render("pages/Recettes", {
-       message: `Modifier la recette ${id}`,
-     });
-   }
 
-   public addRecette() {
     
-     this.response.render("pages/Recettes", {
-       message: "Ajouter une nouvelle recette",     });
-   }
+    public editRecette() {
+      const id = this.request.params.id;
+      this.response.render("pages/Recettes", {
+      data:recettes
+      });
+    }
 
-   public deleteRecette() {
-     const id = this.request.params.id;
-     this.response.render("pages/Recettes", {
-       message: `Supprimer la recette ${id}`,
-     });
-   }
- }
+    public addRecette() {
+    this.response.render("pages/Recettes", { 
+    data:recettes
+          });
+    }
+
+    public deleteRecette() {
+      const id = this.request.params.id;
+      this.response.render("pages/Recettes", {
+       data:recettes
+      });
+    }
+  }
 
--- routes/starter.ts
+++ routes/starter.ts
@@ -3,32 +3,32 @@ import { starterController } from "../Controller/starterController";
 
 const starterRouter = Router();
 
-// Browse
+
 starterRouter.get("/starters", (request, response) => {
   const controller = new starterController(request, response);
   controller.browseRecettes();
    
 });
 
-// Read
+
 starterRouter.get("/starters/:id", (request, response) => {
   const controller = new starterController(request, response);
   controller.readRecette();
 });
 
-// Edit
+
 starterRouter.put("/starters/:id", (request, response) => {
   const controller = new starterController(request, response);
   controller.editRecette();
 });
 
-// Add
+
 starterRouter.post("/starters", (request, response) => {
   const controller = new starterController(request, response);
   controller.addRecette();
 });
 
-// Delete
+
 starterRouter.delete("/starters/:id", (request, response) => {
   const controller = new starterController(request, response);
   controller.deleteRecette();
